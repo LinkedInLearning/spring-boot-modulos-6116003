17f6bb9873b11c9337fb8167490b5dcb1bba7303
Mariona Nadal <school@dsrroma.es>
2025-08-18 11:20:00 +0200

    01_01. Presentación funcional del proyecto base

---
+++ src/test/java/es/dsrroma/school/springboot/integracionbase/ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dsrroma.school.springboot.integracionbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
