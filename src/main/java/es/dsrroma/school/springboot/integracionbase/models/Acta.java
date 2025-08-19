721ec482ba9515316a70ed2f650656f9725a1040
Mariona Nadal <school@dsrroma.es>
2025-08-19 16:29:11 +0200

    03_02. Mapeo de relaciones JPA: 1:1 y 1:N (b)

--- src/main/java/es/dsrroma/school/springboot/integracionbase/models/Acta.java
+++ src/main/java/es/dsrroma/school/springboot/integracionbase/models/Acta.java
@@ -1,9 +1,11 @@
 package es.dsrroma.school.springboot.integracionbase.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +16,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Acta {
     @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String contenido;
+	private String contenido;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private Reunion reunion;
 }
--- src/main/java/es/dsrroma/school/springboot/integracionbase/models/Reunion.java
+++ src/main/java/es/dsrroma/school/springboot/integracionbase/models/Reunion.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,6 +28,5 @@ public class Reunion {
 	private Instant fecha;
 
 	@ManyToOne
-	@JoinColumn
 	private Sala sala;
 }
