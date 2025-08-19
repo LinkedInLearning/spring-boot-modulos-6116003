ab6ac8f53cd74e1ea407a6af32e8576d71166617
Mariona Nadal <school@dsrroma.es>
2025-08-19 16:18:33 +0200

    03_02. Mapeo de relaciones JPA: 1:1 y 1:N (a)

--- src/main/java/es/dsrroma/school/springboot/integracionbase/models/Reunion.java
+++ src/main/java/es/dsrroma/school/springboot/integracionbase/models/Reunion.java
@@ -6,20 +6,29 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor // sin este todo compila, pero hibernate no funciona
 public class Reunion {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NonNull
 	private Long id;
+	@NonNull
 	private String asunto;
+	@NonNull
 	private Instant fecha;
 
+	@ManyToOne
+	@JoinColumn
+	private Sala sala;
 }
