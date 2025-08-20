5080c2b28fca7aa5f37f31fab1a383ad0d52a59e
Mariona Nadal <school@dsrroma.es>
2025-08-20 09:01:20 +0200

    03_06. Endpoints para completar las entidades

--- src/main/java/es/dsrroma/school/springboot/integracionbase/controllers/ReunionController.java
+++ src/main/java/es/dsrroma/school/springboot/integracionbase/controllers/ReunionController.java
@@ -2,7 +2,10 @@ package es.dsrroma.school.springboot.integracionbase.controllers;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -19,17 +22,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import es.dsrroma.school.springboot.integracionbase.models.Acta;
+import es.dsrroma.school.springboot.integracionbase.models.Persona;
 import es.dsrroma.school.springboot.integracionbase.models.Reunion;
+import es.dsrroma.school.springboot.integracionbase.models.Sala;
+import es.dsrroma.school.springboot.integracionbase.repositories.ActaRepository;
+import es.dsrroma.school.springboot.integracionbase.repositories.PersonaRepository;
 import es.dsrroma.school.springboot.integracionbase.repositories.ReunionRepository;
+import es.dsrroma.school.springboot.integracionbase.repositories.SalaRepository;
 
 @RestController
 @RequestMapping("/reuniones")
 public class ReunionController {
 
 	private final ReunionRepository reunionRepository;
+	private final SalaRepository salaRepository;
+	private final PersonaRepository personaRepository;
+	private final ActaRepository actaRepository;
 
-	private ReunionController(ReunionRepository reunionRepository) {
+	private ReunionController(ReunionRepository reunionRepository, SalaRepository salaRepository,
+			PersonaRepository personaRepository, ActaRepository actaRepository) {
 		this.reunionRepository = reunionRepository;
+		this.salaRepository = salaRepository;
+		this.personaRepository = personaRepository;
+		this.actaRepository = actaRepository;
 	}
 
 	@GetMapping("/{requestedId}")
@@ -86,5 +102,95 @@ public class ReunionController {
 		} else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+
+	@PutMapping("/{reunionId}/sala/{salaId}")
+	private ResponseEntity<Void> addSalaToReunion(@PathVariable Long reunionId, 
+			@PathVariable String salaId) {
+		Optional<Reunion> reunionOpt = reunionRepository.findById(reunionId);
+
+		if (reunionOpt.isEmpty()) {
+			return ResponseEntity.notFound().build(); // Si no se encuentra la reunión
+		}
+
+		Reunion reunion = reunionOpt.get();
+
+		Sala sala = salaRepository.findById(salaId).orElse(null);
+		if (sala == null) {
+			return ResponseEntity.notFound().build(); // Si no se encuentra la sala
+		}
+		reunion.setSala(sala);
+
+		reunionRepository.save(reunion);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{reunionId}/acta")
+	private ResponseEntity<Void> addActaToReunion(@PathVariable Long reunionId, 
+			@RequestBody Acta actaRequest) {
+		// Buscar la reunión
+		Optional<Reunion> reunionOpt = reunionRepository.findById(reunionId);
+
+		if (reunionOpt.isEmpty()) {
+			// Si no se encuentra la reunión, devolver 404
+			return ResponseEntity.notFound().build();
+		}
+
+		Reunion reunion = reunionOpt.get();
+
+		Acta acta;
+
+		if (actaRequest.getId() != null) { // acta existente
+			acta = actaRepository.findById(actaRequest.getId()).orElse(null);
+			if (acta == null) {
+				return ResponseEntity.notFound().build();
+			}
+		} else { // acta nueva
+			acta = new Acta();
+			acta.setContenido(actaRequest.getContenido());
+		}
+		acta = actaRepository.save(acta);
+
+		reunion.setActa(acta);
+
+		reunionRepository.save(reunion);
+
+		return ResponseEntity.noContent().build();
+	}
+    
+	/**
+	 * Para añadir participantes a una reunión existente.
+	 * 
+	 * @param reunionId        id de la reunión
+	 * @param participantesIds conjunto de ids de los participantes
+	 * @return
+	 */
+	@PutMapping("/{reunionId}/participantes")
+	private ResponseEntity<Void> addParticipantes(@PathVariable Long reunionId,
+			@RequestBody Set<Long> participantesIds) {
+		Optional<Reunion> reunionOpt = reunionRepository.findById(reunionId);
+
+		if (reunionOpt.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		Reunion reunion = reunionOpt.get();
+
+		Set<Persona> participantes = participantesIds.stream()
+				.map(personaId -> personaRepository.findById(personaId).orElse(null))
+				.filter(Objects::nonNull)
+				.collect(Collectors.toSet());
+
+		if (participantes.size() != participantesIds.size()) {
+			// si hay menos participantes de los esperados
+			return ResponseEntity.notFound().build();
+		}
+
+		reunion.getParticipantes().addAll(participantes);
+
+		reunionRepository.save(reunion);
+
+		return ResponseEntity.noContent().build();
 	}
 }
