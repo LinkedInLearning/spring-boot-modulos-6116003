20fb7f89ee394e6ae92185a39e3ce5a9bd12fa3c
Mariona Nadal <school@dsrroma.es>
2025-08-21 11:25:46 +0200

    06_03. JSON Web Tokens (JWT)

--- src/main/java/es/dsrroma/school/springboot/integracionbase/seguridad/SecurityConfig.java
+++ src/main/java/es/dsrroma/school/springboot/integracionbase/seguridad/SecurityConfig.java
@@ -3,57 +3,55 @@ package es.dsrroma.school.springboot.integracionbase.seguridad;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import es.dsrroma.school.springboot.integracionbase.seguridad.jwt.JwtAuthenticationFilter;
 
 @Configuration
 class SecurityConfig {
-	
-    private final UsuarioDetailsServiceImpl usuarioDetailsService;
 
-    public SecurityConfig(UsuarioDetailsServiceImpl usuarioDetailsService) {
-        this.usuarioDetailsService = usuarioDetailsService;
-    }
+
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(request -> request
-				.requestMatchers("/auth/login").permitAll()
-//				.requestMatchers(
-//	                    "/v3/api-docs/**",
-//	                    "/swagger-ui/**",
-//	                    "/swagger-ui.html",
-//	                    "/reuniones/**",
-//	                    "/actas/**",
-//	                    "/personas/**",
-//	                    "/salas/**").authenticated())
-				.anyRequest().authenticated()
-				)
-				.httpBasic(Customizer.withDefaults())
-				.csrf(csrf -> csrf.disable())
-				.formLogin(form -> form.permitAll());
-		return http.build();
+	    http
+	        .csrf(csrf -> csrf.disable())
+	        .sessionManagement(session -> session
+	        	.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	        )
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers(
+	                "/auth/login",
+	                "/v3/api-docs/**",
+	                "/swagger-ui/**",
+	                "/swagger-ui.html"
+	            ).permitAll()
+	            .anyRequest().authenticated()
+	        )
+	        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+	    return http.build();
 	}
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) 
-    		throws Exception {
-        return config.getAuthenticationManager();
-    }
-	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) 
+			throws Exception {
+		return config.getAuthenticationManager();
+	}
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-//	@Bean
-//	UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
-//		User.UserBuilder users = User.builder();
-//		UserDetails admin = users.username("admin").password(passwordEncoder.encode("abc123")).build();
-//		return new InMemoryUserDetailsManager(admin);
-//	}
 }
